cd9770886930c9d70017af5c995b708dbe133bbf
Karl <krab.berger@gmail.com>
2016-12-22 14:17:52 -0500

    Adding starter test spec.  Currently failing, want to test Travis.

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,15 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Data.Maybe
+import Test.Hspec
+import IceAndFire
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+    describe "getBookById" $ do
+        it "returns a book if id is valid" $ do
+            book <- getBookById 1
+            book `shouldSatisfy` isJust
+        it "returns None if id is invalid" $ do
+            book <- getBookById 100
+            book `shouldSatisfy` isNothing
